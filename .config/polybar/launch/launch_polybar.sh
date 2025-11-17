03a20cad270cfbe2b9773922c581a7249706c866
Skye Purchase <s.purchase25@imperial.ac.uk>
2025-11-17 11:05:20 +0000

    Improve polybar launch

--- .config/polybar/launch/launch_polybar.sh
+++ .config/polybar/launch/launch_polybar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+killall -q polybar
 if type "xrandr" >/dev/null 2>&1; then
     for m in $(xrandr --query | grep " connected" | awk '{print $1}'); do
         LOG_FILE="$HOME/.config/polybar/$m.log"
